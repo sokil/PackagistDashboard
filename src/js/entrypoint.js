bc79fe693a9f1385fbe0463c7a59f1b92fd36901
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-08-10 23:26:27 +0300

    load package list

--- src/js/entrypoint.js
+++ src/js/entrypoint.js
@@ -5,19 +5,24 @@ import VendorPackagesList from '../jsx/VendorPackagesList.jsx';
 
 ReactDOM.render(<Layout/>, document.getElementById('app'));
 
-const packages = [
-    {
-        name: 'sokil/php-mongo',
-        downloadStat: {
-            'daily': 10,
-            'monthly': 20,
-            'total': 30,
-        }
-    }
-];
+fetch("https://packagist.org/packages/list.json?vendor=sokil")
+    .then(response => response.json())
+    .then(response => {
+        const packages = response.packageNames.map(packageName => {
+            return {
+                name: packageName,
+                downloadStat: {
+                    'daily': 10,
+                    'monthly': 20,
+                    'total': 30,
+                }
+            };
+        });
 
-ReactDOM.render(
-    <VendorPackagesList packages={packages}/>,
-    document.getElementById('vendorPackagesContainer')
-);
+        ReactDOM.render(
+            <VendorPackagesList packages={packages}/>,
+            document.getElementById('vendorPackagesContainer')
+        );
+    });
+
 
