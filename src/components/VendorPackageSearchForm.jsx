6f87fdc7f53f49ea93e4fb7dbe9d6fd00b6d9ff5
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-10-27 18:57:17 +0300

    use vendor name from form

--- src/components/VendorPackageSearchForm.jsx
+++ src/components/VendorPackageSearchForm.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
 class VendorPackageSearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            vendor: null
+        };
+    }
+
     submit(e) {
         e.preventDefault();
-        this.props.showVendorPackages('sokil');
+
+        if (this.state.vendor) {
+            this.props.showVendorPackages(this.state.vendor);
+        }
+    }
+
+    changeVendor(e) {
+        this.state.vendor = e.target.value;
     }
 
     render() {
         return <form className="my-2" onSubmit={this.submit.bind(this)}>
                 <div className="input-group input-group-lg">
-                    <input type="text" className="form-control" placeholder="Vendor" />
+                    <input type="text" className="form-control" placeholder="Vendor" onChange={this.changeVendor.bind(this)}/>
                 </div>
             </form>
     }
