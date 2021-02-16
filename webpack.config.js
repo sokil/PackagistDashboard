4cda03e010f2d76a0e2eaaeb0c1cb08046e3155d
Dmytro Sokil <dmytro.sokil@gmail.com>
2021-02-16 23:36:04 +0200

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // set mode
@@ -10,17 +10,28 @@ const mode = process.env.NODE_ENV
 
 console.log('Current mode: ' + '\x1b[36m' + mode + '\x1b[0m');
 
-//less plugin
-const extractLess = new ExtractTextPlugin({
-    filename: "bundle.css",
-    allChunks: true,
-    disable: process.env.NODE_ENV === "development"
-});
+var plugins = [
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: './src/html/index.html',
+                to: './index.html'
+            }
+        ]
+    }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode),
+    })
+];
+
+if (mode !== "production") {
+    plugins.push(new MiniCssExtractPlugin());
+}
 
 // webpack config
 module.exports = {
     mode: mode,
-    entry: ['babel-polyfill', './src/app/entrypoint.js'],
+    entry: ['./src/app/entrypoint.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: './js/bundle.js'
@@ -31,7 +42,7 @@ module.exports = {
         //jquery: "jQuery",
     },
     devtool: mode === "development"
-        ? 'cheap-module-eval-source-map'
+        ? 'cheap-module-source-map'
         : 'source-map',
     module: {
         rules: [
@@ -45,41 +56,44 @@ module.exports = {
             },
             {
                 test: /.jsx?$/,
-                loader: 'babel-loader',
                 exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        node: true,
+                                    },
+                                },
+                            ],
+                            '@babel/preset-react',
+                            '@babel/preset-flow',
+                        ],
+                        plugins: [
+                            '@babel/plugin-syntax-dynamic-import',
+                            '@babel/plugin-syntax-import-meta',
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-proposal-private-methods',
+                            '@babel/plugin-proposal-nullish-coalescing-operator',
+                            '@babel/plugin-proposal-function-bind',
+                            '@babel/plugin-proposal-optional-chaining',
+                            '@babel/plugin-proposal-json-strings',
+                        ]
+                    }
+                },
             },
             {
                 test: /\.(sass|css)$/,
-                use: extractLess.extract({
-                    use: [
-                        {
-                            loader: "css-loader"
-                        },
-                        {
-                            loader: "sass-loader"
-                        }
-                    ],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
+                use: [
+                    mode === 'production' ? MiniCssExtractPlugin.loader : "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]
     },
-    plugins: [
-        new CopyWebpackPlugin(
-            [
-                {from: './src/html/index.html', to: './index.html'}
-            ],
-            {
-                debug: mode === 'production' ? 'warning' : 'info'
-            }
-        ),
-        extractLess,
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(mode),
-        })
-    ]
+    plugins: plugins
 };
